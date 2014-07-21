d83dc2c99f89dbdedcdbe4c883113c0d74aec220
Tim Baumann <tim@timbaumann.info>
2014-07-22 01:15:38 +0200

    Add the engine-io-yesod project

---
+++ engine-io-yesod/src/Network/EngineIO/Yesod.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
+module Network.EngineIO.Yesod (yesodAPI) where
+
+import Control.Applicative
+import Data.Maybe (maybeToList)
+import Control.Arrow (second)
+import Data.Text (pack)
+import Data.Conduit (($$))
+import Data.Conduit.Lift (runCatchC)
+import Data.Conduit.Attoparsec (sinkParser)
+import Data.Monoid (mappend)
+import Control.Monad (unless)
+
+import qualified Data.ByteString.Builder as Builder
+import qualified Network.EngineIO as EIO
+import qualified Yesod.Core as YC
+import qualified Data.HashMap.Strict as HashMap
+import qualified Network.Wai as WAI
+import qualified Network.Wai.Handler.WebSockets as WaiWS
+import qualified Network.WebSockets as WS
+import Network.HTTP.Types.Status as St
+
+--------------------------------------------------------------------------------
+-- | A drop in 'EIO.ServerAPI' that works in Yesod's 'Handler' monad.
+yesodAPI :: (YC.MonadHandler m, YC.MonadBaseControl IO m) => EIO.ServerAPI m
+yesodAPI = EIO.ServerAPI
+  { EIO.srvTerminateWithResponse = \code ct builder -> do
+      let status = filter ((==) code . St.statusCode) [St.status100..St.status511]
+      case status of
+        [] -> error "not a valid status code"
+        (st:_) -> YC.sendResponseStatus st $ YC.TypedContent ct $
+                    YC.toContent $ Builder.toLazyByteString builder
+
+  , EIO.srvGetQueryParams = HashMap.fromListWith (++) . map (second maybeToList)
+                              . WAI.queryString <$> YC.waiRequest
+
+  , EIO.srvParseRequestBody = \p -> do
+      val <- YC.rawRequestBody $$ runCatchC (sinkParser p)
+      case val of
+        Left e  -> YC.invalidArgs ["could not parse request body: " `mappend` pack (show e)]
+        Right v -> return v
+
+  , EIO.srvGetRequestMethod = WAI.requestMethod <$> YC.waiRequest
+
+  , EIO.srvRunWebSocket = \app -> do
+      req <- YC.waiRequest
+      unless (WaiWS.isWebSocketsReq req) $ YC.invalidArgs ["not a websocket request"]
+      YC.sendRawResponseNoConduit $ \src sink ->
+        YC.liftIO $ WaiWS.runWebSockets WS.defaultConnectionOptions
+          (WaiWS.getRequestHead req) app src sink
+  }
