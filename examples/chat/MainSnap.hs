bb16f966fc89ca2bf5f9d2ae973f37d061ce6a22
Tim Baumann <tim@timbaumann.info>
2014-07-22 01:17:57 +0200

    chat example: add variant using Yesod instead of Snap

---
+++ examples/chat/MainSnap.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Chat (server, ServerState (..))
+import Control.Applicative
+
+import qualified Network.EngineIO.Snap as EIOSnap
+import qualified Control.Concurrent.STM as STM
+import qualified Snap.Core as Snap
+import qualified Snap.Util.FileServe as Snap
+import qualified Snap.Http.Server as Snap
+import qualified Network.SocketIO as SocketIO
+
+import Paths_chat (getDataDir)
+
+main :: IO ()
+main = do
+  state <- ServerState <$> STM.newTVarIO 0
+  socketIoHandler <- SocketIO.initialize EIOSnap.snapAPI (server state)
+  dataDir <- getDataDir
+  Snap.quickHttpServe $
+    Snap.route [ ("/socket.io", socketIoHandler)
+               , ("/", Snap.serveDirectory dataDir)
+               ]
